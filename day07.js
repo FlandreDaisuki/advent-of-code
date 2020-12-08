490e5a4adb6bca2bc5bffbc03b64e6328b2ae1cd
FlandreDaisuki <vbnm123c@gmail.com>
2020-12-09 00:26:46 +0800

    Flatten bag mapping can make it easier

--- day07.js
+++ day07.js
@@ -1,66 +1,37 @@
 const inputLines = document.body.textContent.split('\n').filter(Boolean);
 
-const bags = inputLines.map((line) => {
-  const [bag, ...specs] = line.split(/(contain|,)/).filter((s) => s.includes('bag')).map((s) => s.replace(/bag.*/, '').trim());
-  const canContains = specs.map((sp) => sp.split(/(?<=\d+)\s+/)).map(([count, name]) => [name, Number(count)]);
-  return [bag, canContains.some((c) => !c[1]) ? [] : canContains];
-});
+const mapping = inputLines.map((line) => {
+  const [txtFront, txtBack] = line.split('contain');
+  const bag = txtFront.replace('bags', '').trim();
+  const others = txtBack.match(/(\d+) (\w+ \w+)(?= bags?)/g);
 
-const reverseBags = bags.reduce((prev, curr) => {
-  const [bag, beContained] = curr;
-  for (const [name, _] of beContained) {
-    if (!prev[name]) {
-      prev[name] = [];
-    }
-    prev[name].push(bag);
-  }
-  return prev;
-}, {});
+  return others?.map((other) => {
+    const [n, color] = other.trim().match(/(\d+) (.*)/).slice(1);
+    return [bag, color, Number(n)];
+  }) ?? [[bag, null, 0]];
 
-const track = (targets, set = new Set) => {
-  if (!targets.length) { return set.size; }
-  const next = [];
-  for (const tar of targets) {
-    const containers = reverseBags[tar];
-    if (containers) {
-      for (const con of containers) {
-        if (!set.has(con)) {
-          next.push(con);
-          set.add(con);
-        }
-      }
-    }
-  }
-  return track(next, set);
+}).flat(1);
+
+const bagsContain = (target, set = new Set) => {
+  if (set.has(target)) { return set; }
+  set.add(target);
+
+  const founds = mapping.filter((m) => m[1] === target).map((m) => m[0]);
+  if (!founds.length) { return set; }
+
+  return founds.reduce((prev, curr) => bagsContain(curr, prev), set);
 };
 
-track(['shiny gold']); // answer 1
+bagsContain('shiny gold').size - 1; // answer 1
 
-const counterBags = ((bags) => {
-  const mutableBags = [...bags];
-  const map = new Map();
-  bags.filter((bag) => !bag[1].length).forEach((smallestBag) => {
-    map.set(smallestBag[0], 1);
-    const i = mutableBags.findIndex((b) => b[0] === smallestBag[0]);
-    mutableBags.splice(i, 1);
-  });
+const sum = (arr) => arr.reduce((a, b) => a + b);
+const bagsContainWithCount = (target) => {
+  if (!target) { return 0; }
 
-  while (mutableBags.length) {
-    for (const mb of mutableBags) {
-      const [name, canContains] = mb;
-      for (const [i, con] of Object.entries(canContains)) {
-        if (Array.isArray(con) && map.has(con[0])) {
-          canContains[i] = map.get(con[0]) * con[1];
-        }
-      }
-      if (canContains.every((n) => typeof n === 'number')) {
-        map.set(name, canContains.reduce((a, b) => a + b) + 1);
-        const i = mutableBags.findIndex((b) => b[0] === name);
-        mutableBags.splice(i, 1);
-      }
-    }
-  }
-  return map;
-})(bags);
+  const total = mapping
+    .filter((m) => m[0] === target)
+    .map((m) => bagsContainWithCount(m[1]) * m[2]);
+  return sum(total) + 1;
+};
 
-counterBags.get('shiny gold') - 1; // answer 2
+bagsContainWithCount('shiny gold') - 1; // answer 2
