933246c4434c1277b66c8caf8d8fd2e9ed4a9a26
FlandreDaisuki <vbnm123c@gmail.com>
2020-12-23 22:43:48 +0800

    Add day23-2? (bad brute force)
    <del>Maybe I'll get answer after few days</del>

--- day23.js
+++ day23.js
@@ -7,28 +7,31 @@ const mutRotate = (mut) => {
   mut.push(mut.shift());
   return mut;
 };
+const findDist = (target, sequence) => {
+  if (sequence.includes(target)) {
+    return target;
+  }
+  const lesser = sequence.filter((e) => e < target);
+  return lesser.length > 0 ? lesser.reduce((a, b) => Math.max(a, b)) : sequence.reduce((a, b) => Math.max(a, b));
+};
+const rotateUntil = (label, sequence) => {
+  const i = sequence.indexOf(label);
+  return sequence.slice(i).concat(sequence.slice(0, i));
+};
 const move = (sequence, count) => {
   const mutSequence = [...sequence];
-  const findDist = (target) => {
-    if (mutSequence.includes(target)) {
-      return target;
-    }
-    const lesser = mutSequence.filter((e) => e < target);
-    return lesser.length > 0 ? Math.max(...lesser) : Math.max(...mutSequence);
-  };
   /* eslint-disable-next-line no-unused-vars */
   for (const _ of seq(count)) {
     const pickup = mutSequence.splice(1, 3);
     const current = mutSequence[0];
-    const dist = findDist(current - 1);
+    const dist = findDist(current - 1, mutSequence);
     mutSequence.splice(mutSequence.indexOf(dist) + 1, 0, ...pickup);
     mutRotate(mutSequence);
   }
   return mutSequence;
 };
 
-const part1 = move(sequence, 100);
-while (part1[0] !== 1) {
-  mutRotate(part1);
-}
-part1.slice(1).join(''); //answer 1
+rotateUntil(1, move(sequence, 100)).slice(1).join(''); //answer 1
+
+const bigSequence = sequence.concat(seq(1000000 - sequence.length, (i) => i + 1 + sequence.length));
+rotateUntil(1, move(bigSequence, 10000000)).slice(1, 3).reduce((a, b) => a * b); // maybe answer 2 ???????????????
