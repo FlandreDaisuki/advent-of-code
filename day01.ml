3cbcae3197b969f403369c8c62404b7532441945
FlandreDaisuki <vbnm123c@gmail.com>
2021-08-18 00:35:02 +0800

    Add day01-2

--- day01.ml
+++ day01.ml
@@ -19,9 +19,34 @@ let stream_fold f stream init =
   !result
 
 let line_stream = line_stream_of_channel (open_in "day01.txt")
-
 let int_stream = stream_map int_of_string line_stream
-
 let answer1 = stream_fold ( + ) int_stream 0
 
 let () = Printf.printf "answer 1: %d\n" answer1
+
+let cycle items =
+  let buf = ref [] in
+  let next i =
+    if !buf = [] then buf := items;
+    match !buf with
+      | h :: t -> (buf := t; Some h)
+      | [] -> None in
+  Stream.from next
+
+module IntSet = Set.Make(Int)
+
+let rec withdraw_until_seen out_stream seen sum =
+  let s = sum + (Stream.next out_stream) in
+    if IntSet.mem s seen then s
+    else withdraw_until_seen out_stream (IntSet.add s seen) s
+
+let line_stream = line_stream_of_channel (open_in "day01.txt")
+let int_stream = stream_map int_of_string line_stream
+let int_list = stream_fold (fun i acc -> acc @ [i]) int_stream []
+let cycle_int_stream = cycle int_list
+let answer2 = withdraw_until_seen
+  cycle_int_stream
+  (IntSet.singleton 0)
+  0
+
+let () = Printf.printf "answer 2: %d\n" answer2
