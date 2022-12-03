0a7c959376becdc8673917138af445ffe7d79a6f
FlandreDaisuki <vbnm123c@gmail.com>
2022-12-03 10:30:29 +0800

    add explanation to day02

--- day02.js
+++ day02.js
@@ -14,15 +14,15 @@ const sum = (arr) => Array.from(arr).reduce((a, b) => a + b, 0);
 const rounds = getProblemText().split('\n').filter(Boolean);
 
 const FIRST_SCORE_MAP = Object.freeze({
-  'A X': 3 + 1,
-  'A Y': 6 + 2,
-  'A Z': 0 + 3,
-  'B X': 0 + 1,
-  'B Y': 3 + 2,
-  'B Z': 6 + 3,
-  'C X': 6 + 1,
-  'C Y': 0 + 2,
-  'C Z': 3 + 3,
+  'A X': 1 + 3, // 🪨 🪨 draw
+  'A Y': 2 + 6, // 🪨 📜 win
+  'A Z': 3 + 0, // 🪨 ✂️ lose
+  'B X': 1 + 0, // 📜 🪨 lose
+  'B Y': 2 + 3, // 📜 📜 draw
+  'B Z': 3 + 6, // 📜 ✂️ win
+  'C X': 1 + 6, // ✂️ 🪨 win
+  'C Y': 2 + 0, // ✂️ 📜 lose
+  'C Z': 3 + 3, // ✂️ ✂️ draw
 });
 const firstTotalScore = rounds.map((round) => FIRST_SCORE_MAP[round]);
 const answer1 = sum(firstTotalScore);
@@ -31,15 +31,15 @@ console.log('answer1', answer1);
 
 
 const SECOND_SCORE_MAP = Object.freeze({
-  'A X': 3 + 0,
-  'A Y': 1 + 3,
-  'A Z': 2 + 6,
-  'B X': 1 + 0,
-  'B Y': 2 + 3,
-  'B Z': 3 + 6,
-  'C X': 2 + 0,
-  'C Y': 3 + 3,
-  'C Z': 1 + 6,
+  'A X': 3 + 0, // 🪨 ✂️ lose
+  'A Y': 1 + 3, // 🪨 🪨 draw
+  'A Z': 2 + 6, // 🪨 📜 win
+  'B X': 1 + 0, // 📜 🪨 lose
+  'B Y': 2 + 3, // 📜 📜 draw
+  'B Z': 3 + 6, // 📜 ✂️ win
+  'C X': 2 + 0, // ✂️ 📜 lose
+  'C Y': 3 + 3, // ✂️ ✂️ draw
+  'C Z': 1 + 6, // ✂️ 🪨 win
 });
 
 const secondTotalScore = rounds.map((round) => SECOND_SCORE_MAP[round]);
